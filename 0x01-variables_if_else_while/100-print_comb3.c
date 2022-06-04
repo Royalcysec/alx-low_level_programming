da18d5bf56423f364505ce6f1be46ca918b276fd
Royalcysec <tgirlolori@gmail.com>
2022-06-04 13:52:14 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,155 +1,35 @@
 #include <stdio.h>
 
-
-
 /**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-
 int d = 48, a = 48;
 
-
-
 while (d < 58)
-
 {
-
 a = 48;
-
 while (a < 58)
-
 {
-
 if (d != a && d < a)
-
 {
-
 putchar(d);
-
 putchar(a);
-
-
 
 if (!(d == 56 && a == 57))
-
 {
-
-putchar(,);
-
-putchar( );
-
+putchar(',');
+putchar(' ');
 }
-
 }
-
 a++;
-
 }
-
 d++;
-
 }
-
-putchar(n);
-
-
+putchar('\n');
 
 return (0);
-
 }
-
-
-
-Task 11
-
-#include <stdio.h>
-
-
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
-
-{
-
-int d = 48, a = 48, i = 48;
-
-
-
-while (d < 58)
-
-{
-
-a = 48;
-
-while (a < 58)
-
-{
-
-i = 48;
-
-while (i < 58)
-
-{
-
-if (d != a && a != i && d != i && d < a && a < i)
-
-{
-
-putchar(d);
-
-putchar(a);
-
-putchar(i);
-
-if (!(d == 55 && a == 56 && i == 57))
-
-{
-
-putchar(,);
-
-putchar( );
-
-}
-
-}
-
-i++;
-
-}
-
-a++;
-
-}
-
-d++;
-
-}
-
-putchar(n);
-
-
-
-return (0);
-
-}
-
-
